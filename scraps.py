f74a61cdad4b357666dde9c3849349590d01dcbe
TheDucky <dakshkuldeep12@gmail.com>
2022-02-06 23:54:03 +0530

    small fix

--- scraps.py
+++ scraps.py
@@ -1,8 +1,6 @@
-from tkinter import N
-from tkinter.messagebox import NO, YES
 import psutil
 
 if "gnome-terminal-server" in (i.name() for i in psutil.process_iter()):
-    print(YES)
+    print("YES")
 else:
-    print(NO)
+    print("NO")

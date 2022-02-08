ad410e2c32156f1c9e7fb909f59f0276c8a0fd06
TheDucky <dakshkuldeep12@gmail.com>
2022-02-08 21:07:24 +0530

    small code modifications

--- UnixDRP.py
+++ UnixDRP.py
@@ -27,55 +27,95 @@ while True:
     if "gnome-terminal-server" in (i.name() for i in psutil.process_iter()):
 
         if theShell == "/usr/bin/zsh":
-            Slushy.update(state="On GNOME-terminal", details="Using ZSH shell", start=timeOfStart,
-                          large_image="terminal", large_text="GNOME-terminal",
-                          small_image="zsh", small_text="ZSH shell")
+            Slushy.update(state="On GNOME-terminal",
+                          details="Using ZSH shell",
+                          start=timeOfStart,
+                          large_image="terminal",
+                          large_text="GNOME-terminal",
+                          small_image="zsh",
+                          small_text="ZSH shell")
 
         elif theShell == "/usr/bin/bash":
-            Slushy.update(state="On GNOME-terminal", details="Using Bash shell", start=timeOfStart,
-                          large_image="terminal", large_text="GNOME-terminal",
-                          small_image="bash", small_text="Bash shell")
+            Slushy.update(state="On GNOME-terminal",
+                          details="Using Bash shell",
+                          start=timeOfStart,
+                          large_image="terminal",
+                          large_text="GNOME-terminal",
+                          small_image="bash",
+                          small_text="Bash shell")
 
         elif theShell == "/usr/bin/tmux":
-            Slushy.update(state="On GNOME-terminal", details="Using Tmux shell", start=timeOfStart,
-                          large_image="terminal", large_text="GNOME-terminal",
-                          small_image="tmux", small_text="Tmux shell")
+            Slushy.update(state="On GNOME-terminal",
+                          details="Using Tmux shell",
+                          start=timeOfStart,
+                          large_image="terminal",
+                          large_text="GNOME-terminal",
+                          small_image="tmux",
+                          small_text="Tmux shell")
 
         elif theShell == "/usr/bin/fish":
-            Slushy.update(state="On GNOME-terminal", details="Using Fish shell", start=timeOfStart,
-                          large_image="terminal", large_text="GNOME-terminal",
-                          small_image="fish", small_text="Fish shell")
+            Slushy.update(state="On GNOME-terminal",
+                          details="Using Fish shell",
+                          start=timeOfStart,
+                          large_image="terminal",
+                          large_text="GNOME-terminal",
+                          small_image="fish",
+                          small_text="Fish shell")
 
         elif theShell == "/usr/bin/dash":
-            Slushy.update(state="On GNOME-terminal", details="Using Dash shell", start=timeOfStart,
-                          large_image="terminal", large_text="GNOME-terminal",
-                          small_image="dash", small_text="Dash shell")
+            Slushy.update(state="On GNOME-terminal",
+                          details="Using Dash shell",
+                          start=timeOfStart,
+                          large_image="terminal",
+                          large_text="GNOME-terminal",
+                          small_image="dash",
+                          small_text="Dash shell")
 
     if "konsole" in (i.name() for i in psutil.process_iter()):
 
         if theShell == "/usr/bin/zsh":
-            Slushy.update(state="On KDE-Konsole", details="Using ZSH shell", start=timeOfStart,
-                          large_image="konsole", large_text="KDE-Konsolel",
-                          small_image="zsh", small_text="ZSH shell")
+            Slushy.update(state="On KDE-Konsole",
+                          details="Using ZSH shell",
+                          start=timeOfStart,
+                          large_image="konsole",
+                          large_text="KDE-Konsolel",
+                          small_image="zsh",
+                          small_text="ZSH shell")
 
         elif theShell == "/usr/bin/bash":
-            Slushy.update(state="On KDE-Konsole", details="Using Bash shell", start=timeOfStart,
-                          large_image="konsole", large_text="KDE-Konsole",
-                          small_image="bash", small_text="Bash shell")
+            Slushy.update(state="On KDE-Konsole",
+                          details="Using Bash shell",
+                          start=timeOfStart,
+                          large_image="konsole",
+                           arge_text="KDE-Konsole",
+                          small_image="bash",
+                           mall_text="Bash shell")
 
         elif theShell == "/usr/bin/tmux":
-            Slushy.update(state="On KDE-Konsole", details="Using Tmux shell", start=timeOfStart,
-                          large_image="konsole", large_text="KDE-Konsole",
-                          small_image="tmux", small_text="Tmux shell")
+            Slushy.update(state="On KDE-Konsole",
+                          details="Using Tmux shell",
+                          start=timeOfStart,
+                          large_image="konsole",
+                          large_text="KDE-Konsole",
+                          small_image="tmux",
+                          small_text="Tmux shell")
 
         elif theShell == "/usr/bin/fish":
-            Slushy.update(state="On KDE-Konsole", details="Using Fish shell", start=timeOfStart,
-                          large_image="konsole", large_text="KDE-Konsole",
-                          small_image="fish", small_text="Fish shell")
+            Slushy.update(state="On KDE-Konsole",
+                          details="Using Fish shell",
+                          start=timeOfStart,
+                          large_image="konsole",
+                          large_text="KDE-Konsole",
+                          small_image="fish",
+                          small_text="Fish shell")
 
         elif theShell == "/usr/bin/dash":
-            Slushy.update(state="On KDE-Konsole", details="Using Dash shell", start=timeOfStart,
-                          large_image="konsole", large_text="KDE-Konsole",
-                          small_image="dash", small_text="Dash shell")
+            Slushy.update(state="On KDE-Konsole",
+                          details="Using Dash shell",
+                          start=timeOfStart,
+                          large_image="konsole",
+                          large_text="KDE-Konsole",
+                          small_image="dash",
+                          small_text="Dash shell")
 
     time.sleep(15)
--- WindowsDRP.py
+++ WindowsDRP.py
@@ -1,1 +1,39 @@
 # for Windows terminals, CMD and Powershell!
+
+import os
+import time
+import psutil
+from pypresence import Presence
+from dotenv import load_dotenv
+
+load_dotenv()
+LaunchCode = os.getenv("CLIENT_ID") # paste your client id here
+Slushy = Presence(client_id=LaunchCode)
+
+while True: # start the handshake loop!
+    try:
+        Slushy.connect()
+        print("Handshake successfully!")
+        break
+    except:
+        print("ERROR initializing handshake!\nWaiting..." )
+        time.sleep(10)
+
+timeOfStart = time.time() # shows the elapsed time at the bottom (needs to be started in Slushy.update()
+
+while True:
+
+    if "powershell.exe" in (i.name() for i in psutil.process_iter()):
+
+            Slushy.update(state="On Powershell",
+                          start=timeOfStart,
+                          large_image="winterminal",
+                          large_text="Powershell")
+
+    elif "cmd.exe" in (i.name() for i in psutil.process_iter()):
+
+            Slushy.update(state="On CMD",
+                          start=timeOfStart,
+                          large_image="cmd",
+                          large_text="CMD")
+    time.sleep(15)
